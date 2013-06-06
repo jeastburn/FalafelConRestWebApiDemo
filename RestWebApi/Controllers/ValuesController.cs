ced11abcc3730d9b5cc5cdae3efea4560f4f1d96
Josh Eastburn <josh@falafel.com>
2013-06-05 21:12:04 -0400

    ValuesController update

--- RestWebApi/Controllers/ValuesController.cs
+++ RestWebApi/Controllers/ValuesController.cs
@@ -10,9 +10,10 @@ namespace RestWebApi.Controllers
     public class ValuesController : ApiController
     {
         // GET api/values
+        [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value1", "value2", "value3" };
         }
 
         // GET api/values/5
